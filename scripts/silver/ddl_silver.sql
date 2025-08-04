06c99a65f5115d335dcf704837e8fd3d5d4455da
CHENO Gilles <127211623+tcgilles@users.noreply.github.com>
2025-08-04 17:00:05 +0200

    Update ddl_silver.sql
    Changed types of fields 'prd_start_dt' and 'prd_end_dt' of the table silver.crm_prd_info to DATE

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -37,8 +37,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_nm          NVARCHAR(50),
     prd_cost        INT,
     prd_line        NVARCHAR(50),
-    prd_start_dt    DATETIME,
-    prd_end_dt      DATETIME,
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
